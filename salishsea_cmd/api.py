a8f7072227762570971fa6e62d4a9550e255fb47
Doug Latornell <dlatornell@eoas.ubc.ca>
2017-01-08 17:48:02 -0800

    Change to use gather plug-in from NEMO-Cmd package.

--- salishsea_cmd/api.py
+++ salishsea_cmd/api.py
@@ -29,6 +29,7 @@ import cliff.commandmanager
 import yaml
 
 from nemo_cmd import deflate as deflate_plugin
+from nemo_cmd import gather as gather_plugin
 from salishsea_cmd import prepare as prepare_plugin
 
 log = logging.getLogger(__name__)
@@ -109,6 +110,21 @@ def deflate(filenames):
     :param list filenames: Paths/names of files to be deflated.
     """
     return deflate_plugin.deflate(filenames)
+
+
+def gather(results_dir):
+    """Move all of the files and directories from the present working directory
+    into results_dir.
+
+    If results_dir doesn't exist, create it.
+
+    Delete any symbolic links so that the present working directory is empty.
+
+    :param results_dir: Path of the directory into which to store the run
+                        results.
+    :type results_dir: :py:class:`pathlib.Path`
+    """
+    return gather_plugin.gather(results_dir)
 
 
 def prepare(run_desc_file, nemo34=False, nocheck_init=False):
