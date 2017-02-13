2d974bcc8248a1c95e1a76e1e10b0a55349e3417
Doug Latornell <djl@douglatornell.ca>
2017-02-13 13:59:13 -0800

    yapf code clean-ups

--- salishsea_cmd/fspath.py
+++ salishsea_cmd/fspath.py
@@ -12,7 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 """Define the :func:`fspath` function that returns the string representation
 of a file system path.
 This provides a uniform interface for working with :class:`Path` objects in
