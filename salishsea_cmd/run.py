36692f5356252556d7374e5e50528c1c07e37f93
Doug Latornell <dlatornell@eoas.ubc.ca>
2022-03-11 10:58:02 -0800

    Add SPDX short-form license identifiers
    Communicates license information in portable and machine-readable manner.

--- salishsea_cmd/run.py
+++ salishsea_cmd/run.py
@@ -12,6 +12,10 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+
+# SPDX-License-Identifier: Apache-2.0
+
+
 """SalishSeaCmd command plug-in for run sub-command.
 
 Prepare for, execute, and gather the results of a run of the Salish Sea NEMO model.
